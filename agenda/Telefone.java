1a2c5325935222cfecb4cfc1f45d06f2183902be
Rojasspeedcuber <105650416+Rojasspeedcuber@users.noreply.github.com>
2023-04-04 12:14:41 -0300

    Upload da atividade 02

---
+++ agenda/Telefone.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.agenda;
+
+public class Telefone {
+    private String numero;
+    private String tipo;
+    
+    public Telefone(String numero, String tipo){
+        this.numero = numero;
+        this.tipo = tipo;  
+    }
+    
+    public String getTelefone(){
+        return numero + " " + tipo;
+    }
+}
